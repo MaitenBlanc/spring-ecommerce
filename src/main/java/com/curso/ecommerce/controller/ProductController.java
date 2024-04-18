3e1bdccbf905a6ad1c7c2340aa14e0b3da489397
Your Name <you@example.com>
2024-04-18 17:34:03 -0300

    Implementación para guardar la imagen del producto

--- src/main/java/com/curso/ecommerce/controller/ProductController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductController.java
@@ -3,15 +3,15 @@ package com.curso.ecommerce.controller;
 import com.curso.ecommerce.model.Product;
 import com.curso.ecommerce.model.User;
 import com.curso.ecommerce.service.ProductService;
+import com.curso.ecommerce.service.UploadFileService;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @Controller
@@ -22,6 +22,9 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+
+    @Autowired
+    private UploadFileService upload;
 
     @GetMapping("")
     public String show(Model model) {
@@ -35,10 +38,26 @@ public class ProductController {
     }
 
     @PostMapping("/save")
-    public String save(Product product) {
+    public String save(Product product, @RequestParam("img") MultipartFile file) throws IOException {
         logger.info("Este es el objeto producto {}", product);
         User u = new User(1, "", "", "", "", "", "", "");
         product.setUser(u);
+
+        // Para subir imagen al servidor
+        if(product.getId() == null) {     // Cuando se crea un producto
+            String nameImage = upload.saveImage(file);
+            product.setImage(nameImage);
+        } else {
+            if(file.isEmpty()) {    // Cuando se edita el producto pero no se cambia la imagen
+                Product p = new Product();
+                p = productService.get(product.getId()).get();
+                product.setImage(p.getImage());
+            } else {
+                String nameImage = upload.saveImage(file);
+                product.setImage(nameImage);
+            }
+        }
+
         productService.save(product);
         return "redirect:/products";
     }
